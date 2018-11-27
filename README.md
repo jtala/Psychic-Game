bd1839dff06cb72edaeacc8e17aabfcaeaff2cdb
Jonathan Talavera <jtalavera@g.ucla.edu>
2018-11-26 19:56:00 -0800

    Final Game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+
+1) This is an awesome psychic game where the user has to guess what letter the computer is thinking of!.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,81 @@
-//There is a user input and a computer input.
-
 // There is a wins and a loss counter.
 var wins = 0;
 var losses = 0;
 
 // There are 10 guesses to start.
 var guesses = 10;
+
+// There is an array with the user's incorrect guesses.
 var storeGuesses = [];
 
 
 
-// Generating computer's array of letters.
+// Generating computer's array of letters + the letter it stores.
  var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
  var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
- // An event is triggered when the user presses a button.
+ // The game starts when the user presses a button.
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
+
+    // This stores the user's guess into an empty array.
     storeGuesses.push(userGuess);
-    
 
-    // Computer input randomly generates an alphabet letter.
         // Checking what the computer guess is.
+        console.log("The computer is thinking of: "+ compGuess);
 
-        console.log(compGuess);
-
-    //  if user guess === computer guess
+    //  What happens if user guesses correctly.
     if (userGuess === compGuess) {
-
+        // Alert the user that his guess was correct.
+        alert("Yes, " + userGuess +" was what the computer was thinking about!");
         //increase wins by 1
         wins++;
         //reset lives to 10.
         guesses = 10;
         // clears currently stored User Guesses.
         storeGuesses = [];
-         //computer generates new random letter, loop starts over again.
+         //computer generates new random letter to play again.
          compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-        // compGuess;
 
     }
         
        
     else {
 
-         // if user guess is not computer guess, decrease guesses by 1.
+         // if user guess is not computer guess, decrease guesses amount by 1.
         guesses--;
 
-        console.log("Incorrect entry");
-
-        // store the incorrect guess into the userguess
+        // To check if 
+        console.log("Incorrect entry: This isn't the computer's Guess");
         
-         // if lives decrease to 0, add a loss.
+         // What happens if guesses decreases to 0.
          if (guesses === 0) {
+            // Alert the user what the computer was thinking.
+             alert("Nope! The computer was thinking of "+ compGuess +" !");
+
+            // Increase losses by 1.
              losses++;
+
+             //Reset guesses back to 10.
+             guesses = 10;
 
             //Clear storage of guesses.
             storeGuesses = [];
-        
-            // computer genereates new random letter.
-            // compGuess;
 
+            // Sets up a new letter for user to guess.
+            compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
         }
     } 
-    document.getElementById("wins-text").textContent = wins;
-    document.getElementById("losses-text").textContent = losses;
-    document.getElementById("guessesleft-text").textContent = guesses;
-    document.getElementById("storeguesses-text").textContent = storeGuesses;
+    // Display the wins, losses, guess count, and used guesses so far, linking to HTML. Make sure this is within the keyclick event.
+
+    document.getElementById("wins-text").textContent = "Points: " + wins;
+    document.getElementById("losses-text").textContent = "Losses: " + losses;
+    document.getElementById("guessesleft-text").textContent ="You have " + guesses + "  guesses left!";
+    document.getElementById("storeguesses-text").textContent = "Your guesses so far: "+ storeGuesses;
 
 };
-    // Display the wins, losses, guess count, and used guesses so far.
-
-      
-      
-        // winsText.textContent = "wins: " + wins ;
-        // lossesText.textContent = "losses: " + losses;
-        // guessesLeftText.textContent = "Guesses Left: " + guesses;
-        // storeGuessesText.textContent = "Your guesses so far: "+ storeGuesses;
 
 
 
